0e35bce9af58d357c99fa9efd3ca9dbc50b6d7fa
kiiillerbot <shimolkhan.dev@gmail.com>
2022-04-25 00:44:10 +0800

    Basic React Components And Receiving Data To Child Component Via Props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Expenses from './components/Expenses/Expenses.js';
+
 function App() {
+
+  const expenses = [
+    { id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+    { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2021, 5, 12) }
+  ];
+
   return (
+    // Component Map: App -> Expenses -> ExpenseItem -> ExpenseDate
     <div className="App">
-      <h4>Hello from component App.js</h4>
+      <Expenses expenses={ expenses } />
     </div>
   );
 }
