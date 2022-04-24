4cbde37faf21d03cb45ec8a7cd57b101b7e904e7
kiiillerbot <shimolkhan.dev@gmail.com>
2022-04-25 02:27:30 +0800

    Handling Basic Form Submission

--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -3,45 +3,57 @@ import './NewExpenseForm.css';
 
 const NewExpenseForm = () => {
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: ''
-  });
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   const titleChangeHandler = (event) => {
-    setUserInput((prevState) => {
-      return {...prevState, enteredTitle: event.target.value};
-    });
+    setEnteredTitle(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
-    setUserInput((prevState) => {
-      return{...prevState, enteredAmount: event.target.value};
-    });
+    setEnteredAmount(event.target.value);
   };
 
   const dateChangeHandler = (event) => {
-    setUserInput((prevState) => {
-      return{...prevState, enteredDate: event.target.value}
-    });
+    setEnteredDate(event.target.value);
   };
+
+  const submitHandler = (event) => {
+    // Preventing the browser to refresh on submitting the form.
+    event.preventDefault();
+
+    // Creating an object to store all entered data
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+
+    // Output the object in console as tabular form
+    console.table(expenseData);
+
+    // Clearing all the inputs after form submission with the help of useState functions and react two way data binding by adding value property with all the inputs we are listening to.
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  }
 
 
   return (
-    <form>
+    <form onSubmit={ submitHandler }>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={ titleChangeHandler }/>
+          <input type="text" value={ enteredTitle } onChange={ titleChangeHandler }/>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" onChange={ amountChangeHandler }/>
+          <input type="number" value={ enteredAmount } onChange={ amountChangeHandler }/>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2022-04-25" max = "2022-05-25" onChange={ dateChangeHandler }/>
+          <input type="date" value={ enteredDate } min="2022-04-25" max = "2022-05-25" onChange={ dateChangeHandler }/>
         </div>
         <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
