4e13c4b1f84c65d42f245693cb3a99811e015d09
kiiillerbot <shimolkhan.dev@gmail.com>
2022-04-25 23:08:59 +0800

    Render Dynamic Expenses With Filtering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import ExpenseItem from '../ExpenseItem/ExpenseItem.js';
 import ExpensesFilter from '../ExpensesFilter/ExpensesFilter.js';
+import ExpensesList from '../ExpensesList/ExpensesList.js';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -9,36 +9,18 @@ const Expenses = (props) => {
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
-    console.log(selectedYear);
   };
+
+  const filteredExpenses = (props.expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  }))
 
   return(
     <div className="expenses">
-      <div>
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      </div>
-      <ExpenseItem
-        title={ props.expenses[0].title }
-        amount={ props.expenses[0].amount }
-        date={ props.expenses[0].date }
-      />
-      <ExpenseItem
-        title={ props.expenses[1].title}
-        amount={ props.expenses[1].amount }
-        date={ props.expenses[1].date }
-      />
-      <ExpenseItem
-        title={ props.expenses[2].title }
-        amount={ props.expenses[2].amount }
-        date={ props.expenses[2].date }
-      />
-      <ExpenseItem
-        title={ props.expenses[3].title }
-        amount={ props.expenses[3].amount }
-        date= { props.expenses[3].date }
-      />
+      <ExpensesList items={filteredExpenses} />
     </div>
-  )
-}
+  );
+};
 
 export default Expenses;
