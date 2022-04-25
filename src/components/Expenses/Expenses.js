def54270e68f24e9d455a0f40d532ee1817e4b7e
kiiillerbot <shimolkhan.dev@gmail.com>
2022-04-25 13:41:20 +0800

    Added Dropdown Click Listener to Year Select Dropdown

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import ExpenseItem from '../ExpenseItem/ExpenseItem.js';
-import NewExpense from '../NewExpense/NewExpense.js';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter.js'
 import './Expenses.css';
 
 const Expenses = (props) => {
+
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  }
+
   return(
     <div className="expenses">
+      <div>
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      </div>
       <ExpenseItem
         title={ props.expenses[0].title }
         amount={ props.expenses[0].amount }
